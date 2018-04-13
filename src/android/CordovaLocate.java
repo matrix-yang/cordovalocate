d71115c39d96611b85d1bad73a2513c2fb595155
matrix-yang <739260804@qq.com>
2018-04-13 10:06:29 +0800

    修改完成

--- src/android/CordovaLocate.java
+++ src/android/CordovaLocate.java
@@ -56,11 +56,11 @@ public class CordovaLocate extends CordovaPlugin {
                 StringBuffer sb = new StringBuffer();
                 //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
                 if (location.getErrorCode() == 0) {
-                    sb.append("定位成功" + "\n");
-                    sb.append("定位类型: " + location.getLocationType() + "\n");
-                    sb.append("经    度    : " + location.getLongitude() + "\n");
-                    sb.append("纬    度    : " + location.getLatitude() + "\n");
-                    sb.append("精    度    : " + location.getAccuracy() + "米" + "\n");
+                    /*sb.append("定位成功" + "\n");
+                    sb.append("定位类型: " + location.getLocationType() + "\n");*/
+                    sb.append("经    度    : " + location.getLongitude() + ",");
+                    sb.append("纬    度    : " + location.getLatitude() + ",");
+                    /*sb.append("精    度    : " + location.getAccuracy() + "米" + "\n");
                     sb.append("提供者    : " + location.getProvider() + "\n");
 
                     sb.append("速    度    : " + location.getSpeed() + "米/秒" + "\n");
@@ -72,9 +72,8 @@ public class CordovaLocate extends CordovaPlugin {
                     sb.append("市            : " + location.getCity() + "\n");
                     sb.append("城市编码 : " + location.getCityCode() + "\n");
                     sb.append("区            : " + location.getDistrict() + "\n");
-                    sb.append("区域 码   : " + location.getAdCode() + "\n");
-                    sb.append("地    址    : " + location.getAddress() + "\n");
-                    sb.append("兴趣点    : " + location.getPoiName() + "\n");
+                    sb.append("区域 码   : " + location.getAdCode() + "\n");*/
+                    sb.append("地    址    : " + location.getAddress() + ",");
                     //定位完成的时间
                 } else {
                     //定位失败
@@ -83,10 +82,10 @@ public class CordovaLocate extends CordovaPlugin {
                     sb.append("错误信息:" + location.getErrorInfo() + "\n");
                     sb.append("错误描述:" + location.getLocationDetail() + "\n");
                 }
-                sb.append("***定位质量报告***").append("\n");
+                /*sb.append("***定位质量报告***").append("\n");
                 sb.append("* WIFI开关：").append(location.getLocationQualityReport().isWifiAble() ? "开启" : "关闭").append("\n");
                 sb.append("* GPS星数：").append(location.getLocationQualityReport().getGPSSatellites()).append("\n");
-                sb.append("****************").append("\n");
+                sb.append("****************").append("\n");*/
                 //解析定位结果，
                 callbackContext.success(sb.toString());
             } else {
